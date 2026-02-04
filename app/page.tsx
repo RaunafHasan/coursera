a027251087d51d34f2fd01da418ec7dbbd46b3e3
RaunafHasan <raunathassanayan@gmail.com>
2026-02-05 04:51:39 +0600

    Done 0.000

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default async function Home() {
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {coursesWithProgress.map((course) => (
+            {coursesWithProgress.map((course: any) => (
               <CourseCard key={course.id} {...course} />
             ))}
           </div>
